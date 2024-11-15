f1214ab4893bc4db70514522fd9e078afa72e3d3
Optimistic123 <manishkj441@gmail.com>
2024-11-15 21:24:54 +0530

    nav bar correacted

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav>
+    <nav className='nav-container'>
       <div className="nav-header">
         <Link to="/">
           <img src="src/assets/logo.png" alt="Logo" className="logo" />
@@ -20,17 +20,21 @@ const Navbar = () => {
           ☰
         </button>
       </div>
-      
-      {/* Separate nav-menu as a div instead of ul */}
+    
       <div className={`nav-menu ${isMenuOpen ? 'open' : ''}`}>
-        <button className="cross-icon" onClick={toggleMenu}>✖️</button>
-        <ul>
-          <li><Link to="/courses" onClick={() => setIsMenuOpen(false)}>Courses</Link></li>
-          <li><Link to="/services" onClick={() => setIsMenuOpen(false)}>Our Service</Link></li>
-          <li><Link to="/whyus" onClick={() => setIsMenuOpen(false)}>Why Us</Link></li>
-          <li><Link to="/blog" onClick={() => setIsMenuOpen(false)}>Blogs</Link></li>
-          <li><Link to="/contact" onClick={() => setIsMenuOpen(false)}>Contact Us</Link></li>
-        </ul>
+        <div className='smaller-screen-nav-header'>  {/*only visible in smaller screen and in a line*/}
+          <label>Global Vvypar</label>
+          <button onClick={toggleMenu}>✖️</button>
+        </div>
+        <div className='page-link'>      {/*responsive: on Bigger screen in horizontal line and on smaller screen in vertical direction*/}
+          <ul>
+            <li><Link to="/courses" onClick={() => setIsMenuOpen(false)}>Courses</Link></li>
+            <li><Link to="/services" onClick={() => setIsMenuOpen(false)}>Our Service</Link></li>
+            <li><Link to="/whyus" onClick={() => setIsMenuOpen(false)}>Why Us</Link></li>
+            <li><Link to="/blog" onClick={() => setIsMenuOpen(false)}>Blogs</Link></li>
+            <li><Link to="/contact" onClick={() => setIsMenuOpen(false)}>Contact Us</Link></li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
