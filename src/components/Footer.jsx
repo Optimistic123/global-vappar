97341a3e705ed3174ff35e91f3a2b55756ed542d
Optimistic123 <manishkj441@gmail.com>
2024-12-06 16:40:43 +0530

    design change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,49 +1,74 @@
 import React from 'react';
 import { FaFacebook, FaTwitter, FaLinkedin, FaInstagram, FaYoutube } from 'react-icons/fa';
 import './Footer.scss';
+import CTAButton from './CTAButton';
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-container">
-        {/* Social Media Links */}
-        <div className="social-media">
-          <h4>Connect with Us</h4>
-          <div className="social-icons">
-            <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-                <FaFacebook />
-            </a>
-            <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
-                <FaTwitter />
-            </a>
-            <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-                <FaLinkedin />
-            </a>
-            <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
-                <FaInstagram />
-            </a>
-            <a href="https://www.youtube.com/@GlobalVyapar" target="_blank" rel="noopener noreferrer">
-                <FaYoutube />
-            </a>
+        <div className='links-group'>
+          <div className="description">
+            <h4>Global Vyapar</h4>
+            <p>See why businesses across India
+              trust Global Vyapar to solve their
+              most complex export-import
+              challenges.</p>
+          </div>
+          <div className="useful-links-company">
+            <h4>Company</h4>
+            <ul>
+              <li><a href="/">Home</a></li>
+              <li><a href="/about-us">About</a></li>
+              <li><a href="/services">Services</a></li>
+              <li><a href="/courses">Courses</a></li>
+              <li><a href="/blog">Blog</a></li>
+              <li><a href="/contact">Contact</a></li>
+            </ul>
+          </div>
+          {/* Useful Links */}
+          <div className="useful-links-docu">
+            <h4>Documentation</h4>
+            <ul>
+              <li><a href="/about-us">Help Center</a></li>
+              <li><a href="/courses">Contact</a></li>
+              <li><a href="/webinars">FAQ</a></li>
+              <li><a href="/contact">Privacy Policy</a></li>
+            </ul>
+          </div>
+          {/* Social Media Links */}
+          <div className="useful-links-social">
+            <h4>Social</h4>
+            <ul>
+              <li><a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                <FaFacebook size={40} />
+              </a></li>
+              <li>
+                <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+                  <FaTwitter size={40} />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                  <FaLinkedin size={40} />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
+                  <FaInstagram size={40} />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.youtube.com/@GlobalVyapar" target="_blank" rel="noopener noreferrer">
+                  <FaYoutube size={40} />
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
-
-        {/* General Information */}
-        <div className="general-info">
-          <h4>About Global Vyapar</h4>
-          <p>Your trusted partner in mastering the import-export business. Join our webinars and explore our courses to grow your global trade network.</p>
-          <p>&copy; {new Date().getFullYear()} Global Vyapar. All rights reserved.</p>
-        </div>
-
-        {/* Useful Links */}
-        <div className="useful-links">
-          <h4>Quick Links</h4>
-          <ul>
-            <li><a href="/about-us">About Us</a></li>
-            <li><a href="/courses">Explore Courses</a></li>
-            <li><a href="/webinars">Webinars</a></li>
-            <li><a href="/contact">Contact</a></li>
-          </ul>
+        <div class="footer-term-cond">
+          <span>&copy; {new Date().getFullYear()} Global Vyapar. All rights reserved.</span>
+          <a href="/terms-and-conditions" class="footer-link">Terms & Conditions</a>
         </div>
       </div>
     </footer>
